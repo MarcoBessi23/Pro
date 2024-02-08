7ef9134f695b113ca4128fb646a3b5c6218eb428
MarcoBessi23 <marcobessi23@gmail.com>
2024-02-08 22:31:14 +0100

    n

--- Mean_field_aprx.py
+++ Mean_field_aprx.py
@@ -30,8 +30,8 @@ def update_graph(PG, init_infected, J, t):
             if i[state] == infected:
                 s = s + 1
         u = infected_prob(s, k, t, J)
-        r = rd.choice([0, 1], weights=[1-u, u])
-        if r == 1:
+        r = rd.choice([0, 1], weights=[1-u, u])#Forse più semplice np.random.uniform(0,1)
+        if r == 1:                             #r<u
             next_graph.nodes[node][state] = infected
     PG = next_graph
 
@@ -41,14 +41,14 @@ def infected_prob(s, k, t, J):
 
 def information_graphs(PG, VG, q=0.5):
     AP = nx.adjacency_matrix(PG)
-    VG = nx.adjacency_matrix(VG)
+    AG = nx.adjacency_matrix(VG)
     AI = np.matrix()
     for x, y in AP:
         r = np.random.uniform(0,1) # BINOMIAL
         if r < q:
-            AI[x, y] = AP[x, y]
+            AI[x, y] = AG[x, y]
         else:
-            AI[x, y] = VG[x, y]
+            AI[x, y] = AP[x, y]
     IG = nx.from_numpy_matrix(AI)
     return IG
 
@@ -69,5 +69,3 @@ def main():
 
 if __name__ =="__main__":
     main()
-
-
